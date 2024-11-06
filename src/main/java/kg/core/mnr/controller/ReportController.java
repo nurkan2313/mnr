4b5b1bc822ebf2bf6919319b9e3214c61323020f
nurkan <dzhusupov.nurkan@gmail.com>
2024-11-06 12:09:16 +0600

    fix

--- src/main/java/kg/core/mnr/controller/ReportController.java
+++ src/main/java/kg/core/mnr/controller/ReportController.java
@@ -1,22 +1,19 @@
 package kg.core.mnr.controller;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-@Controller
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+
 @AllArgsConstructor
+@Controller
 @RequestMapping("/report")
 public class ReportController {
 
     @GetMapping
     public String report() {
-        return "/report/reports";
+        return "/otchet/reports";
     }
 
 }
